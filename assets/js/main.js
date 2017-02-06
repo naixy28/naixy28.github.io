020f51724aad0ae78c5af4706dfeb05d726210ee
naixy28 <naixy28@gmail.com>
2017-02-07 01:00:53 +0800

    particles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 (function() {
     window.onload=function(){
         $('.loader').css('opacity','0');
+
+        init();
     }
+
+    function init(){
+        initParticles();
+        bindings();
+    }
+
+    function bindings(){}
+
+    function initParticles(){
+        particlesJS.load('particles-js', '/assets/data/particles.json', function() {
+            console.log('callback - particles.js config loaded');
+        });
+    }
+
 
 }).call(this)
