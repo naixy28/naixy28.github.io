9f92f772a92e7a1259742bfac65eb50ff5e68bdc
naixy28 <naixy28@gmail.com>
2017-02-17 22:29:00 +0800

    font test

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,34 @@
         bindings();
     }
 
-    function bindings(){}
+    function bindings(){
+        bgOffestEffect();
+
+    }
 
     function initParticles(){
         particlesJS.load('particles-js', '/assets/data/particles.json', function() {
             console.log('callback - particles.js config loaded');
         });
     }
+
+    function bgOffestEffect(){
+        var offset = {
+            x:0,
+            y:0
+        },
+            scale = 2;
+            bg = document.querySelector('.parallax-bg');
+        document.addEventListener('mousemove', cursorPositionListener, false)
+        function cursorPositionListener(e) {
+            var clientRect = document.querySelector('body').getBoundingClientRect();
+            offset.x = (event.clientX /(clientRect.width/2) -1) * scale;
+            offset.y = (event.clientY /(clientRect.height/2) -1) * scale;
+            bg.style.transformOrigin = (50+offset.x) + '% ' + (50+offset.y) + '%';
+        }
+    }
+
+
 
 
 }).call(this)
