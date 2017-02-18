7a733ec91a3185ff5ac199e206f3a01cbb57b6d2
naixy28 <naixy28@gmail.com>
2017-02-19 03:11:27 +0800

    optimize animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,22 +23,44 @@
 
     function bgOffestEffect(){
         var offset = {x:0, y:0},
-            scale = 8;
+            scale = 4;
             bg = document.querySelector('.parallax-bg');
 
-        // document.addEventListener('mouseenter', cursorEnterScreen, true);
-        // function cursorEnterScreen(e){
-        //
-        // }
-
-        document.addEventListener('mouseenter', cursorPositionListener, false);
-        function cursorPositionListener(e) {
-            //bg.style.transition = "none";
+        document.addEventListener('mousemove', cursorPositionListener, false);
+        function cursorPositionListener(event) {
             var clientRect = document.querySelector('body').getBoundingClientRect();
             offset.x = (event.clientX /(clientRect.width/2) -1) * scale;
             offset.y = (event.clientY /(clientRect.height/2) -1) * scale;
             bg.style.transformOrigin = (50+offset.x) + '% ' + (50+offset.y) + '%';
+            function AnimationWrap() {
+                bg.style.transformOrigin = (50+offset.x) + '% ' + (50+offset.y) + '%';
+            }
+            requestAnimationFrame(AnimationWrap)
         }
+
+
+        /*try requestAnimationFrame*/
+
+        // document.addEventListener('click', clickListener, false);
+        // function clickListener(event) {
+        //     console.log('a');
+        //     var i = 0;
+        //     var clientRect = document.querySelector('body').getBoundingClientRect();
+        //     offset.x = (event.clientX /(clientRect.width/2) -1) * scale / 60;
+        //     offset.y = (event.clientY /(clientRect.height/2) -1) * scale / 60;
+        //
+        //     function AnimationWrap2() {
+        //         i++;
+        //         bg.style.transformOrigin = (50+offset.x*i) + '% ' + (50+offset.y*i) + '%';
+        //         if(i<=60){
+        //             requestAnimationFrame(AnimationWrap2);
+        //         }
+        //     }
+        //     AnimationWrap2();
+        // }
+
+
+
     }
 
 
